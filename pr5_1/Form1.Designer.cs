d8343021a5c0c4a200559ecfb50ee83b124351d6
Андрей Артатомонов <an_drei_artamonov@mail.ru>
2023-10-18 01:53:36 +0500

    практическая 6 часть 1

--- pr5_1/Form1.Designer.cs
+++ pr5_1/Form1.Designer.cs
@@ -29,12 +29,68 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Image = ((System.Drawing.Image)(resources.GetObject("pct.Image")));
+            this.pct.Location = new System.Drawing.Point(12, 12);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(921, 332);
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(260, 307);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(341, 307);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // tmr
+            // 
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1815, 335);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pct);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer tmr;
     }
 }
 
--- pr5_1/Form1.cs
+++ pr5_1/Form1.cs
@@ -16,5 +16,47 @@ namespace pr5_1
         {
             InitializeComponent();
         }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            /*Увеличивает значение свойства Left изображения pct на 20 пикселей
+              Проверяет, если текущая позиция изображения pct, плюс его ширина и 20 пикселей, превышает ширину текущей формы*/
+            pct.Left = pct.Left + 20;
+            if (pct.Left + pct.Width + 20 > this.Width)
+            {
+                tmr.Stop();
+                pct.Left = 0;
+                btnStart.Text = "Старт";
+                return;
+            }
+        }
+        //Логическая переменная, отражающая текущее состояние анимации
+        //(true, если анимация выполняется, false, если анимация остановлена)
+        private bool isAnimating = false;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (isAnimating)
+            {
+                //возвращает изображение в начальное положение
+                //(устанавливает значение свойства Left изображения pct в 0 пикселей)
+                tmr.Stop();
+                pct.Left = 0;
+                btnStart.Text = "Старт";
+                isAnimating = false;
+            }
+            else
+            {
+                tmr.Start();
+                btnStart.Text = "Стоп";
+                //Устанавливает значение переменной(выполняется)
+                isAnimating = true;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
