3d8ca2a88e095294e70fbc985c3aad0d18526967
Андрей Артатомонов <an_drei_artamonov@mail.ru>
2023-10-18 01:09:58 +0500

    практическа 5 часть 2

--- pr5_1/Form1.Designer.cs
+++ pr5_1/Form1.Designer.cs
@@ -29,12 +29,111 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSeconds = new System.Windows.Forms.TextBox();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.tmrSecundomer = new System.Windows.Forms.Timer(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnComputer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(224, 93);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "min";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(264, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "sec";
+            // 
+            // txtSeconds
+            // 
+            this.txtSeconds.Location = new System.Drawing.Point(158, 49);
+            this.txtSeconds.Name = "txtSeconds";
+            this.txtSeconds.Size = new System.Drawing.Size(100, 20);
+            this.txtSeconds.TabIndex = 3;
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.Location = new System.Drawing.Point(11, 49);
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(100, 20);
+            this.txtMinutes.TabIndex = 4;
+            // 
+            // tmrSecundomer
+            // 
+            this.tmrSecundomer.Tick += new System.EventHandler(this.tmrSecundomer_Tick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(224, 122);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // btnComputer
+            // 
+            this.btnComputer.Location = new System.Drawing.Point(224, 149);
+            this.btnComputer.Name = "btnComputer";
+            this.btnComputer.Size = new System.Drawing.Size(75, 23);
+            this.btnComputer.TabIndex = 6;
+            this.btnComputer.Text = "computer";
+            this.btnComputer.UseVisualStyleBackColor = true;
+            this.btnComputer.Click += new System.EventHandler(this.btnComputer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(383, 195);
+            this.Controls.Add(this.btnComputer);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.txtSeconds);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "секундомер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSeconds;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.Timer tmrSecundomer;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnComputer;
     }
 }
 
--- pr5_1/Form1.cs
+++ pr5_1/Form1.cs
@@ -12,9 +12,66 @@ namespace pr5_1
 {
     public partial class Form1 : Form
     {
+        private int seconds = 0;
+        private int minutes = 0;
+        
+
         public Form1()
-        {
+        {//задается интервал для таймера tmrSecundomer
             InitializeComponent();
+            tmrSecundomer.Interval = 1000;
+         //добавляет обработчик события Tick для таймера tmrSecundomer.
+         //Когда происходит событие Tick, будет вызываться метод tmrSecundomer_Tick
+            tmrSecundomer.Tick += new EventHandler(tmrSecundomer_Tick);
+        }
+
+        private void tmrSecundomer_Tick(object sender, EventArgs e)
+        {
+          //Внутри этого метода происходит увеличение счетчика секунд и минут,
+          //а также обновление текстовых полей txtSeconds и txtMinutes на форме.
+            seconds++;
+
+            if (seconds == 60)
+            {
+                minutes++;
+                seconds = 0;
+            }
+
+            txtSeconds.Text = seconds.ToString();
+            txtMinutes.Text = minutes.ToString();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Если таймер включен, он отключает его и меняет текст кнопки на "Start".
+            //Если таймер выключен, он включает его и меняет текст кнопки на "Stop"
+            if (tmrSecundomer.Enabled)
+            {
+                tmrSecundomer.Enabled = false;
+                btnStart.Text = "Start";
+            }
+            else
+            {
+                tmrSecundomer.Enabled = true;
+                btnStart.Text = "Stop";
+            }
+            
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            //сброс значений счетчиков секунд и минут в ноль, а также обновление текстовых полей txtSeconds
+            //и txtMinutes на форме, чтобы отобразить новые значения
+            seconds = 0;
+            minutes = 0;
+            txtSeconds.Text = "0";
+            txtMinutes.Text = "0";
+        }
+
+        private void btnComputer_Click(object sender, EventArgs e)
+        {
+            //создает окно сообщения MessageBox и отображает текущее время (часы, минуты, секунды) в формате "HH:mm:ss"
+            MessageBox.Show(DateTime.Now.ToString("HH:mm:ss"));
         }
     }
 }
