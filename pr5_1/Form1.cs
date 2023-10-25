4d2cb4bc9559e7082a68153a208bf229f9c6f511
Андрей Артатомонов <an_drei_artamonov@mail.ru>
2023-10-25 23:16:56 +0500

    класс

--- pr5_1/Form1.cs
+++ pr5_1/Form1.cs
@@ -16,5 +16,31 @@ namespace pr5_1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Считывание значений из текстового поля textBox
+            double x = Convert.ToDouble(textBox1.Text);
+            double accuracy = Convert.ToDouble(textBox2.Text);
+
+            double sum = 0;
+            double slog = x;
+            int colslog = 1;
+            // Выполняеются расчеты суммы ряда, пока значение слагаемого больше или равно заданной точности
+            while (Math.Abs(slog) >= accuracy)
+            {
+                // Добавляем текущее слагаемое к общей сумме
+                sum += slog;
+                // Увеличиваем количество слагаемых на 1
+                colslog++;
+                // Вычисляем новое значение слагаемого
+                slog = x / colslog;
+            }
+           // Вывод результатов 
+            label4.Text = "sum = " + sum.ToString();
+            label5.Text = "quanity" + colslog.ToString();
+        }
+
+     
     }
 }
