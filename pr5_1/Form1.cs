69285157725e9948ce45396b7eab5432f69c380d
Андрей Артатомонов <an_drei_artamonov@mail.ru>
2023-10-30 23:55:17 +0500

    часть 1_7

--- pr5_1/Form1.cs
+++ pr5_1/Form1.cs
@@ -16,5 +16,65 @@ namespace pr5_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Показывает диалог открытия файла. Если пользователь нажмет кнопку ОК, загрузится, выбранная картинка
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+                info.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Показывает диалоговое окно цвета. Если пользователь нажмет кнопку ОК, изменится, на выбранный фон 
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Если флажок растянуть,меняется PictureBox
+            // Свойству SizeMode присвоено значение «Растянуть». Если пользователь очищает флажок, меняет его на стандартный
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void savefile_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
     }
 }
