8ab9002fea7ebb12f69b8736caff3b8d672602c4
Андрей Артатомонов <an_drei_artamonov@mail.ru>
2023-11-13 23:39:05 +0500

    ну сделал и сделал

--- pr5_1/Form1.cs
+++ pr5_1/Form1.cs
@@ -19,8 +19,10 @@ namespace pr5_1
         private int NumberSymbols(string stroka, char symbol)
         {
             int k = 0;
+            //проверка каждого символа в строке 
             for (int i = 0; i<stroka.Length; i++)
             {
+                //если текущий символ равен заданному символу, увеличиваем счетчик
                 if (stroka[i] == symbol)
                 {
                     k += 1;
@@ -30,12 +32,18 @@ namespace pr5_1
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {  
+        {
+            //получаем входную строку из текстового поля
             string input = textBox1.Text;
+            //получаем символ из текстового поля и берем первый символ (если есть конечно)
             char symbol = textBox4.Text[0];
+            //вызываем не аркадия паровозова конечно, но метод по подсчету количества заданного символа в строке
             int count = NumberSymbols(input, symbol);
+            //вывод в textBox (такая панелька на форме)
             textBox2.Text = count.ToString();
+            //даем переменную для сочетания "ма"
             int maCount = 0;
+            //запускаем супер мега цикл, который проходит по каждому символу в строке, проверяя пары символов "м" и "а"
             for (int i = 0; i < input.Length - 1; i++)
             {
                 if (input[i] == 'м' && input[i +1] == 'а')
@@ -43,6 +51,7 @@ namespace pr5_1
                     maCount++;
                 }
             }
+            //выводим в textBox (такая панелька на форме)
             textBox3.Text = maCount.ToString();  
         }
     }
