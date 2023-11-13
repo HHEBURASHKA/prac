51cf87d40eebb88848ad6dcd91631776b00b3966
Артём Соломатов <artemka2004sol@gmail.com>
2023-11-13 15:56:44 +0500

    попка номер 9

--- pr5_1/Form1.cs
+++ pr5_1/Form1.cs
@@ -16,5 +16,35 @@ namespace pr5_1
         {
             InitializeComponent();
         }
+        private int NumberSymbols(string stroka, char symbol)
+        {
+            int k = 0;
+            for (int i = 0; i<stroka.Length; i++)
+            {
+                if (stroka[i] == symbol)
+                {
+                    k += 1;
+                }
+            }
+            return k;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            string input = textBox1.Text;
+            char symbol = textBox4.Text[0];
+            int count = NumberSymbols(input, symbol);
+            textBox2.Text = count.ToString();
+            int maCount = 0;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == 'м' && input[i +1] == 'а')
+                {
+                    maCount++;
+                }
+            }
+            textBox3.Text = maCount.ToString();  
+        }
     }
+
 }
